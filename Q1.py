64959871ba72ed8e127cb0c10e075cc33d4b0600
Marcoszikk <58007418+Marcoszikk@users.noreply.github.com>
2019-11-20 16:48:38 -0300

    Create Q1.py
    PROVA COPIADA

---
+++ Q1.py
@@ -0,0 +1,17 @@
+
+
+
+#A)
+def datasplit(data):
+  dataint = {}
+  dataint = data.split('/')
+  dataint = {"ANO": 0, "MES": 1, "DIA": 2}
+ 
+#C)
+def removetwopoint(filestr):
+  lista = []
+  lista = filsestr.split(':')
+  return lista
+
+#E)
+def main(escolha):
